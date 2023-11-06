3dafabf22a093b6b43e71fade7315b586b282115
JawadProgrammer <JawadProgrammer@gmail.com>
2023-11-07 01:35:05 +0500

    change routes

--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -2,14 +2,17 @@ import { useContext } from "react";
 import Auth from "../pages/Auth";
 import Chat from "../pages/Chat";
 import { UserContext } from "./UserContext";
+import Cookies from "js-cookie";
 
 const Routes = () => {
+  const token = Cookies.get("token");
+
   const { userData } = useContext(UserContext);
 
-  if (userData) {
+  if (token) {
     return <Chat />;
   }
-  
+
   return <Auth />;
 };
 
