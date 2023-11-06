ef1101de3160c38f29e36cfe92d55bac56ce2846
JawadProgrammer <JawadProgrammer@gmail.com>
2023-11-07 01:15:25 +0500

    again changes again

--- server/app.js
+++ server/app.js
@@ -23,10 +23,13 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.set("trust proxy", 1);
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    optionsSuccessStatus:200
   })
 );
 
