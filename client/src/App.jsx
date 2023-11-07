a3c6818b8ca744bd2e9e68f51233ede399abffca
JawadProgrammer <JawadProgrammer@gmail.com>
2023-11-07 20:05:45 +0500

    change credentials

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
-import axios from "axios";
 import { UserContextProvider } from "./components/UserContext";
 import Routes from "./components/Routes";
-
-axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
--- server/app.js
+++ server/app.js
@@ -23,13 +23,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
-
+app.use(cors());
 
 app.use(helmet());
 
