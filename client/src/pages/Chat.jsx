958f6f950f8fa803d7f93a198c201b0f7a6db4d2
JawadProgrammer <JawadProgrammer@gmail.com>
2023-11-07 20:09:11 +0500

    change ws url

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -24,7 +24,7 @@ const Chat = () => {
   const { userData } = useContext(UserContext);
 
   const connectToWs = () => {
-    const ws = new WebSocket("ws://localhost:4000");
+    const ws = new WebSocket("ws://https://mern-chat-app-1-server.vercel.app");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", (event) => {
